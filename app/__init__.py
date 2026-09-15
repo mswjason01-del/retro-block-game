91292ddca24fee1953631ac5fb9a52e662b3305b
Jason Coc <mswjason01@gmail.com>
2026-09-15 00:34:15 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,10 @@ def create_app(test_config=None):
     db.init_app(app)
 
     from . import models  # noqa: F401
+
+    with app.app_context():
+    db.create_all()
+
     from .auth import auth_bp
     from .routes import main_bp
     app.register_blueprint(auth_bp)
